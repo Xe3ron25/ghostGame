c1fb14f8d0b6be32c8ac85381e22660e51d1c246
Xe3ron25 <ctrakestraw2@gmail.com>
2024-10-25 16:10:40 -0400

    speed up + random speed update
    merged the random speed and speed up systems

--- GhostGame.py
+++ GhostGame.py
@@ -1,11 +1,26 @@
 import pygame
-import sys
 import random
 pygame.init()
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((800,800))
 artifact = pygame.Rect((380,380,20,20))
+
+camPos = pygame.Rect(pygame.Rect(0, 0, 20, 20))
+color = (255, 0, 0)
+
+font = pygame.font.Font(None, 32)
+font2 = pygame.font.Font(None, 64)
+start_time = pygame.time.get_ticks()
+speed_interval = 5000
+last_speed = pygame.time.get_ticks()
+
+ghostSpeed1 = 2
+ghostSpeed2 = 2
+ghostSpeed3 = 2
+ghostSpeed4 = 2
+
+ghost_speed = 0
 
 run = True
 lives = 3
@@ -30,50 +45,53 @@ ghost = spawn_ghost_x(1)
 ghost2 = spawn_ghost_x(2)
 ghost3 = spawn_ghost_y(1)
 ghost4 = spawn_ghost_y(2)
-#default speed
-ghostSpeed1 = 2
-ghostSpeed2 = 2
-ghostSpeed3 = 2
-ghostSpeed4 = 2
-#speedName = ['slow1', 'med2', 'fast3']
-#speedProb = [0.1,0.1,0.8] #[0.2,0.7,0.1]
 
-def move_towards(target, ghost, ghostSpeed):
-
+def move_towards(target, ghost, ghost_speed):
     if ghost.x <= target.x:
-        ghost.x += ghostSpeed
+        ghost.x += ghost_speed
     elif ghost.x >= target.x + target.width:
-        ghost.x -= ghostSpeed
+        ghost.x -= ghost_speed
 
     if ghost.y <= target.y:
-        ghost.y += ghostSpeed
+        ghost.y += ghost_speed
     elif ghost.y >= target.y + target.height:
-        ghost.y -= ghostSpeed
-
-        #speedChoice = random.choices(speedName, speedProb)
-        #if speedChoice == 'slow1':
-        #    ghostSpeed = 1
-        #elif speedChoice == 'med2':
-        #    ghostSpeed = 2
-        #elif speedChoice == 'fast3':
-        #    ghostSpeed = 10
-
+        ghost.y -= ghost_speed
 
 while run:
+    elapsed_time = (pygame.time.get_ticks() - start_time) / 1000
+    timer_text = font.render(f'Time: {int(elapsed_time)}s', True, (255, 255, 255))
+    lives_text = font.render(f"Lives:  {int(lives)}", True, (255,255,255))
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 run = False
+        if event.type == pygame.MOUSEMOTION:
+            pos = pygame.mouse.get_pos()
+            camPos.x = pos[0]
+            camPos.y = pos[1]
 
 
-    #if run:
-    move_towards(artifact, ghost, ghostSpeed1)
-    move_towards(artifact, ghost2, ghostSpeed2)
-    move_towards(artifact, ghost3, ghostSpeed3)
-    move_towards(artifact, ghost4, ghostSpeed4)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            color = (0, 255, 0)
+            flash = pygame.draw.circle(screen, (255, 255, 255), (camPos.x, camPos.y), 30)
+
+        # unclick
+        if event.type == pygame.MOUSEBUTTONUP:
+            color = (255, 0, 0)
+
+    current_time = pygame.time.get_ticks()
+    if current_time - last_speed >= speed_interval:
+        ghost_speed = ghost_speed + .2
+        last_speed = current_time
+
+    if run:
+        move_towards(artifact, ghost, ghostSpeed1)
+        move_towards(artifact, ghost2, ghostSpeed2)
+        move_towards(artifact, ghost3, ghostSpeed3)
+        move_towards(artifact, ghost4, ghostSpeed4)
 
     if ghost.colliderect(artifact) == True:
-        #lives -= 1
+        lives -= 1
         print("Lives: " + str(lives))
         ghost = spawn_ghost_x(1)
 
@@ -81,17 +99,17 @@ while run:
         print(speedChoice)
         if speedChoice <= 3:
             print("1 slow")
-            ghostSpeed1 = 1
+            ghostSpeed1 = 1 + ghost_speed
         elif 4 <= speedChoice <= 9:
             print("1 med")
-            ghostSpeed1 = 3
+            ghostSpeed1 = 2 + ghost_speed
         elif speedChoice == 10:
             print("1 fast")
-            ghostSpeed1 = 5
+            ghostSpeed1 = 3 + ghost_speed
 
         move_towards(artifact, ghost, ghostSpeed1)
     if ghost2.colliderect(artifact) == True:
-        #lives -= 1
+        lives -= 1
         print("Lives: " + str(lives))
         ghost2 = spawn_ghost_x(2)
 
@@ -99,17 +117,17 @@ while run:
         print(speedChoice)
         if speedChoice <= 3:
             print("2 slow")
-            ghostSpeed2 = 1
+            ghostSpeed2 = 1 + ghost_speed
         elif 4 <= speedChoice <= 9:
             print("2 med")
-            ghostSpeed = 3
+            ghostSpeed = 2 + ghost_speed
         elif speedChoice == 10:
             print("2 fast")
-            ghostSpeed2 = 5
+            ghostSpeed2 = 3 + ghost_speed
 
         move_towards(artifact, ghost2, ghostSpeed2)
     if ghost3.colliderect(artifact) == True:
-        #lives -= 1
+        lives -= 1
         print("Lives: " + str(lives))
         ghost3 = spawn_ghost_y(1)
 
@@ -117,17 +135,17 @@ while run:
         print(speedChoice)
         if speedChoice <= 3:
             print("3 slow")
-            ghostSpeed3 = 1
+            ghostSpeed3 = 1 + ghost_speed
         elif 4 <= speedChoice <= 9:
             print("3 med")
-            ghostSpeed3 = 3
+            ghostSpeed3 = 2 + ghost_speed
         elif speedChoice == 10:
             print("3 fast")
-            ghostSpeed3 = 5
+            ghostSpeed3 = 3 + ghost_speed
 
         move_towards(artifact, ghost3, ghostSpeed3)
     if ghost4.colliderect(artifact) == True:
-        #lives -= 1
+        lives -= 1
         print("Lives: " + str(lives))
         ghost4 = spawn_ghost_y(1)
 
@@ -135,24 +153,104 @@ while run:
         print(speedChoice)
         if speedChoice <= 3:
             print("4 slow")
-            ghostSpeed4 = 1
+            ghostSpeed4 = 1 + ghost_speed
         elif 4 <= speedChoice <= 9:
             print("4 med")
-            ghostSpeed4 = 3
+            ghostSpeed4 = 2 + ghost_speed
         elif speedChoice == 10:
             print("4 fast")
-            ghostSpeed4 = 5
+            ghostSpeed4 = 3 + ghost_speed
 
         move_towards(artifact, ghost4, ghostSpeed4)
     if lives <= 0:
-        run = False
+        pass
+#camera reaction
+    if camPos.colliderect(ghost) == True:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            ghost = spawn_ghost_x(1)
 
+            speedChoice = random.randint(1, 10)
+            print(speedChoice)
+            if speedChoice <= 3:
+                print("1 slow")
+                ghostSpeed1 = 1 + ghost_speed
+            elif 4 <= speedChoice <= 9:
+                print("1 med")
+                ghostSpeed1 = 2 + ghost_speed
+            elif speedChoice == 10:
+                print("1 fast")
+                ghostSpeed1 = 3 + ghost_speed
+
+            move_towards(artifact, ghost, ghostSpeed1)
+
+    if camPos.colliderect(ghost2) == True:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            ghost2 = spawn_ghost_x(2)
+
+            speedChoice = random.randint(1, 10)
+            print(speedChoice)
+            if speedChoice <= 3:
+                print("2 slow")
+                ghostSpeed2 = 1 + ghost_speed
+            elif 4 <= speedChoice <= 9:
+                print("2 med")
+                ghostSpeed = 2 + ghost_speed
+            elif speedChoice == 10:
+                print("2 fast")
+                ghostSpeed2 = 3 + ghost_speed
+
+            move_towards(artifact, ghost2, ghostSpeed2)
+
+    if camPos.colliderect(ghost3) == True:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            ghost3 = spawn_ghost_y(1)
+
+            speedChoice = random.randint(1, 10)
+            print(speedChoice)
+            if speedChoice <= 3:
+                print("3 slow")
+                ghostSpeed3 = 1 + ghost_speed
+            elif 4 <= speedChoice <= 9:
+                print("3 med")
+                ghostSpeed3 = 2 + ghost_speed
+            elif speedChoice == 10:
+                print("3 fast")
+                ghostSpeed3 = 3 + ghost_speed
+
+            move_towards(artifact, ghost3, ghostSpeed3)
+
+    if camPos.colliderect(ghost4) == True:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            ghost4 = spawn_ghost_y(2)
+
+            speedChoice = random.randint(1, 10)
+            print(speedChoice)
+            if speedChoice <= 3:
+                print("4 slow")
+                ghostSpeed4 = 1 + ghost_speed
+            elif 4 <= speedChoice <= 9:
+                print("4 med")
+                ghostSpeed4 = 2 + ghost_speed
+            elif speedChoice == 10:
+                print("4 fast")
+                ghostSpeed4 = 3 + ghost_speed
+
+            move_towards(artifact, ghost4, ghostSpeed4)
 
     screen.fill((0, 0, 0))
+    screen.blit(timer_text, (0, 0))
+    screen.blit(lives_text, (700,0))
     pygame.draw.rect(screen,(255,0,0) ,artifact)
     pygame.draw.rect(screen, (255,255,255), ghost)
     pygame.draw.rect(screen, (255, 255, 255), ghost2)
     pygame.draw.rect(screen, (255, 255, 255), ghost3)
     pygame.draw.rect(screen, (255, 255, 255), ghost4)
+    cameraPosition = pygame.draw.rect(screen, color, (camPos))
     pygame.display.flip()
     clock.tick(60)
+
+screen.fill((0, 0, 0))
+final_time_text = font2.render(f'You lasted: {int(elapsed_time)}s', True, (255, 255, 255))
+screen.blit(final_time_text, (290, 400))
+pygame.display.flip()
+pygame.time.delay(2000)
